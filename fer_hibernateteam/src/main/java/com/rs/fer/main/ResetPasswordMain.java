e3c1f9840505387f4b67255b0707605ed075c477
NANI <NANI@vinod>
2020-01-30 04:08:03 -0500

    reset password

--- fer_hibernateteam/src/main/java/com/rs/fer/main/ResetPasswordMain.java
+++ fer_hibernateteam/src/main/java/com/rs/fer/main/ResetPasswordMain.java
@@ -9,7 +9,7 @@ public class ResetPasswordMain {
 	public static void main(String[] args) {
 		FERService ferservice = new FERServiceImpl();
 
-		boolean isReset = ferservice.resetPassword(1, "admin", "naga1");
+		boolean isReset = ferservice.resetPassword(1, "naga1", "admin");
 
 		if (isReset) {
 			System.out.println("password changed successfully");
