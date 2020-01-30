79b19f04eb71ac4b4f7fd1f67fecc44ab5eab57a
anila <anila@DESKTOP-MGEJETD>
2020-01-30 15:49:06 +0530

    I made changes in editexpense

--- fer_hibernateteam/src/main/java/com/rs/fer/main/EditExpenseMain.java
+++ fer_hibernateteam/src/main/java/com/rs/fer/main/EditExpenseMain.java
@@ -13,14 +13,14 @@ public class EditExpenseMain {
 
 		Expense expense = new Expense();
 
-		expense.setId(11);
+		expense.setId(2);
 		expense.setExpensetype("co");
 		expense.setDate("26/12/2019");
 		expense.setPrice(400);
 		expense.setNoofItems(2);
 		expense.setTotal(300);
 		expense.setBywhom("jen");
-		expense.setUserid(8);
+		expense.setUserid(2);
 
 		boolean isEditExpense = ferservice.editExpense(expense);
 		if (isEditExpense) {
